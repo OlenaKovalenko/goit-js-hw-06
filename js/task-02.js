dfa9ecd907962ef63908fc6f377e154ab39235f4
OlenaKovalenko <elenkow17@gmail.com>
2023-08-23 23:06:35 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+const markup = ingredients.map(item => {
+  const li = document.createElement('li');
+  li.textContent = item;
+  li.classList.add('item');
+  return li;
+})
+
+list.append(...markup);
+console.dir(list);
