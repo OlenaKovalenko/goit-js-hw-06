f34dc75e7ccb3414d442b42df3bc30bed6c1ac7e
OlenaKovalenko <elenkow17@gmail.com>
2023-08-25 23:04:13 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,12 @@ const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
 
-nameInput.addEventListener('input', event => {
-    if (nameInput.value) {
-        nameOutput.textContent = event.currentTarget.value;
-    }
-})
+nameInput.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+
+    const inputValue = event.currentTarget.value;
+
+    nameOutput.textContent = inputValue ? inputValue : "Anonymous";
+
+}
