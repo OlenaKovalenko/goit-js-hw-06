fd6dc1b1871922ec43f5d9f6dd1820af84b63ce3
OlenaKovalenko <elenkow17@gmail.com>
2023-08-26 20:50:33 +0300

    update task-6

--- js/task-06.js
+++ js/task-06.js
@@ -3,8 +3,10 @@ const inputSymbols = document.querySelector('#validation-input');
 inputSymbols.addEventListener('blur', () => {
     
     if (inputSymbols.value.length == inputSymbols.dataset.length) {
+        inputSymbols.classList.remove("invalid");
         inputSymbols.classList.add("valid");
     } else {
+        inputSymbols.classList.remove("valid");
         inputSymbols.classList.add("invalid");
     }
 })
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const btn = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+const body = document.querySelector('body');
+
+btn.addEventListener('click', onButtonClick);
+
+function onButtonClick(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = body.getAttribute('style').split(":").splice(1, 1).join('').slice(0, -1);
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
