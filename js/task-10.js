925e8e034d1be09a5bb2572a10a75fa9dcd23ed5
OlenaKovalenko <elenkow17@gmail.com>
2023-08-26 23:16:29 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,45 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputNumberEl = document.querySelector("input");
+
+const btnCreateEl = document.querySelector("[data-create]");
+btnCreateEl.addEventListener('click', getAmount);
+
+const btnDestroyEl = document.querySelector("[data-destroy]");
+btnDestroyEl.addEventListener('click', destroyBoxes);
+
+const divBoxesEl = document.querySelector("#boxes");
+
+
+function getAmount() {
+  const amount = inputNumberEl.value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+
+let size = 30;
+const arrayBoxes = [];
+
+  for (let i = 0; i <= amount; i += 1) {
+  
+  const box = document.createElement("div");
+
+  box.style.width = `${size}px`;
+  box.style.height = `${size}px`;
+  box.style.backgroundColor = getRandomHexColor();
+  
+  size += 10;
+  arrayBoxes.push(box);
+ }
+
+divBoxesEl.append(...arrayBoxes);
+}
+
+
+function destroyBoxes() {
+  divBoxesEl.innerHTML = "";
+  inputNumberEl.value = "";
+}
